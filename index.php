09fcff44a3b53fae990c0fb5df265329728c7d49
Antendis <86629893+Antendis@users.noreply.github.com>
2023-02-04 23:19:34 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,37 +1,18 @@
 <!DOCTYPE html>
-<html>
-  <head>
+<html lang="en">
+<head>
     <meta charset="UTF-8">
-    <title><?php echo $pageTitle; ?></title>
-    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700&display=swap" rel="stylesheet">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
     <link rel="stylesheet" href="style.css">
-  </head>
-  <body>
-    <header>
-      <div class="logo">
-        <h1><?php echo $siteTitle; ?></h1>
-      </div>
-      <nav>
-        <ul>
-          <li><a href="#about">About</a></li>
-          <li><a href="#projects">Projects</a></li>
-          <li><a href="#contact">Contact</a></li>
-        </ul>
-      </nav>
-      <div class="menu-toggle">
-        <i class="fas fa-bars"></i>
-      </div>
-    </header>
+    <script defer src="app.js"></script>
 
-    <main>
-      <?php echo $pageContent; ?>
-    </main>
+</head>
+<body>
+    
+    <h1>Hi mum!</h1>
 
-    <footer>
-      <p>&copy; <?php echo date('Y'); ?> <?php echo $siteTitle; ?></p>
-    </footer>
-
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <script src="script.js"></script>
-  </body>
+</body>
 </html>
