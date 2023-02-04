5347d7bd0d00eaae110455a31bc78e4e5d4b004a
Antendis <86629893+Antendis@users.noreply.github.com>
2023-02-04 20:29:05 +0000

    Update script.js

--- script.js
+++ script.js
@@ -16,3 +16,21 @@ for (let i = 0; i < 5; i++) {
 // Generate a random location for the food
 function generateFood() {
   const x = Math.floor(Math.random() * game
+
+// Handle arrow key inputs
+document.addEventListener("keydown", function(event) {
+  switch (event.keyCode) {
+    case 37: // Left arrow
+      direction = "left";
+      break;
+    case 38: // Up arrow
+      direction = "up";
+      break;
+    case 39: // Right arrow
+      direction = "right";
+      break;
+    case 40: // Down arrow
+      direction = "down";
+      break;
+  }
+});
