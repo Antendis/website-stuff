6ff80d92d19b4496c62e386ca7b2c94e484f561e
Antendis <86629893+Antendis@users.noreply.github.com>
2023-02-04 20:07:57 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log("JavaScript is connected!");
+window.addEventListener('scroll', function() {
+  let scrollPos = window.scrollY;
+  let animationContainer = document.querySelector('.animation-container');
+  let man = document.querySelector('.man');
+  let football = document.querySelector('.football');
+  
+  man.style.transform = `translateX(${scrollPos/10}%) translateY(${scrollPos/20}%)`;
+  football.style.transform = `translateX(${scrollPos/5}%) translateY(-${scrollPos/10}%)`;
+});
