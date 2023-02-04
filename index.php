42d149f27095db83ca9c04d93ebd8bf1daaf0360
Antendis <86629893+Antendis@users.noreply.github.com>
2023-02-04 19:45:05 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
-<?php
-
-echo "massiv ball";
+<html>
+  <head>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <script src="script.js"></script>
+  </head>
+  <body>
+    <?php 
+      echo "Hello World";
+    ?>
+  </body>
+</html>
