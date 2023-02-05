5739a18bc3ae5b8384d3100c953e055eac138100
Antendis <86629893+Antendis@users.noreply.github.com>
2023-02-05 18:24:50 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,12 @@
 <!DOCTYPE html>
+<!DOCTYPE html>
 <html>
   <head>
     <title>Hello Mother!</title>
     <link rel="stylesheet" type="text/css" href="style.css">
   </head>
   <body>
+    <h1>Welcome to my site</h1>
     Hello Mother.
     <script src="script.js"></script>
   </body>
