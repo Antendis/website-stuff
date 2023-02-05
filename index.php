1ec32ba2e8f624c631e54c3ae1db845843971ebc
Antendis <86629893+Antendis@users.noreply.github.com>
2023-02-05 04:20:30 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,18 +1,23 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-
-    <link rel="stylesheet" href="style.css">
-    <script defer src="app.js"></script>
-
-</head>
-<body>
-    
-    <h1>Hi mum!</h1>
-
-</body>
+<html>
+  <head>
+    <style>
+      body {
+        background-color: #333;
+        display: flex;
+        align-items: center;
+        height: 100vh;
+      }
+      h1 {
+        color: #fff;
+        margin: 0;
+        font-size: 36px;
+        text-align: center;
+      }
+    </style>
+  </head>
+  <body>
+    <h1>Hello mum!</h1>
+  </body>
 </html>
+
