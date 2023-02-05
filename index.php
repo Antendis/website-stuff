d6cba1af3bf470ffdea9e5b25c4e7492ef90b38e
Antendis <86629893+Antendis@users.noreply.github.com>
2023-02-05 16:34:35 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+<title>PHP Test</title>
 </head>
 <body>
-    <h1>Hi mum!</h1>
+<?php echo '<p>Hello World</p>'; ?>
 </body>
 </html>
