4cea4e19311f30c737e82f7b8a96d4720904f657
Antendis <86629893+Antendis@users.noreply.github.com>
2023-02-05 19:47:27 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,37 @@
 <!DOCTYPE html>
-<!DOCTYPE html>
 <html>
   <head>
-    <title>Hello Mother!</title>
+    <title>Rohail's Web Dev Testing</title>
     <link rel="stylesheet" type="text/css" href="style.css">
   </head>
   <body>
-    <h1>Welcome to my site</h1>
-    Hello Mother.
     <script src="script.js"></script>
+
+    <section class="hidden">
+      <h1>Welcome to my site!</h1>
+      Hello Mother.
+    </section>
+
+    <section class="hidden">
+      This is my website.
+    </section>
+
+    <section class="hidden">
+      It's not very good yet.
+    </section>
+
+    <section class="hidden">
+      But it will be.
+    </section>
+
+    <section class="hidden">
+      Look forward to it:)
+    </section>
+
+    <section class="hidden">
+      (I forgot to empty the dishwasher)
+    </section>
+
+
   </body>
 </html>
