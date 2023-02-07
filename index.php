24a6e5f47ebcf71ba855b70a7c11334d161a149d
Antendis <86629893+Antendis@users.noreply.github.com>
2023-02-07 16:48:28 +0000

    Update index.php
    made it look nicer in last css update and added my name is name

--- index.php
+++ index.php
@@ -1,19 +1,29 @@
 <!DOCTYPE html>
 <html>
   <head>
+    <!------------------METADATA------------------>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
+
+    <!------------------Title n page links------------------>
     <title>Rohail's Web Dev Testing</title>
     <link rel="stylesheet" type="text/css" href="style.css">
     <script defer src="script.js"></script>
   </head>
+  
+  <!------------------Stuff------------------>
   <body>
+    <div id="header">
+      <div id="HiMyNameIs">
+        <p1>Hi, my name is</p1>
+      </div>
+
+      <h1>Rohail Sheikh</h1>
+    </div>
 
     <section class="hidden">
-      <h1>Welcome to my site!</h1>
-      Hello Mother.
+      (Hello Mother.)
     </section>
 
     <section class="hidden">
@@ -35,7 +45,5 @@
     <section class="hidden">
       (I forgot to empty the dishwasher)
     </section>
-
-
   </body>
 </html>
