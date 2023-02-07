a77b804415cca60d2efa43df03744649521b70ce
Antendis <86629893+Antendis@users.noreply.github.com>
2023-02-07 13:45:54 +0000

    Update index.php

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="refresh" content="20">
     
     <title>Rohail's Web Dev Testing</title>
     <link rel="stylesheet" type="text/css" href="style.css">
@@ -13,7 +14,7 @@
 
     <section class="hidden">
       <h1>Welcome to my site!</h1>
-      (Hello Mother.)
+      Hello Mother.
     </section>
 
     <section class="hidden">
