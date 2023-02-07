5b349e0642ccca67c00a6f40af65fd63d679d9f7
Antendis <86629893+Antendis@users.noreply.github.com>
2023-02-07 20:11:34 +0000

    Update index.php
    staggered hero text

--- index.php
+++ index.php
@@ -5,7 +5,6 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
     <!------------------Title n page links------------------>
     <title>Rohail's Web Dev Testing</title>
     <link rel="stylesheet" type="text/css" href="style.css">
@@ -15,43 +14,41 @@
   <!------------------Stuff------------------>
   <body onload="window.scrollTo(0,0)">
 
-    <div id="hiMyNameIs">
+      <div id="himynameis" class="hidden">
+        Hi, my name is
+      </div>
 
-      Hi, my name is
+      <div id="bigname" class="hidden">
+        <h1>
+          Rohail Sheikh.
+        </h1>
+      </div>
 
-    </div>
+      <div id="shortsummary" class="hidden">
+        <h1>
+          I like to learn to do things.
+        </h1>
+      </div>
 
-    <div id="header">
+      <section id="summary" class="hidden">
+        (Hello Mother.)
+      </section>
 
-      <h1>Rohail Sheikh</h1>
-
-    </div>
-
-    <section id="subheader" class="hidden">
-
-      (Hello Mother.)
-
-    </section>
 
     <section class="hidden">
       This is my website.
     </section>
-
     <section class="hidden">
       It's not very good yet.
     </section>
-
     <section class="hidden">
       But it will be.
     </section>
-
     <section class="hidden">
       Look forward to it:)
     </section>
-
     <section class="hidden">
       (I forgot to empty the dishwasher)
     </section>
   </body>
 </html>
-
