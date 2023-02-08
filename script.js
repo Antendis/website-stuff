3d3be74517e06fef7a2b6edfe0e8949aea203e3d
Antendis <86629893+Antendis@users.noreply.github.com>
2023-02-08 19:11:38 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -16,3 +16,27 @@ hiddenElements.forEach((el) => observer.observe(el));
 window.onload = function() {
     window.scrollTo(0, 0);
   };
+
+//RIBBON
+var header = document.getElementById("ribbon");
+var prevScrollpos = window.pageYOffset;
+
+header.style.boxShadow = "none";
+
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    header.style.top = "0";
+  } else {
+    header.style.top = "-80px";
+  }
+  if (currentScrollPos === 0) {
+    header.style.boxShadow = "none";
+    header.style.marginTop = "10px";
+  } else {
+    header.style.boxShadow = "0 4px 4px rgba(0, 0, 0, 0.4)";
+    header.style.marginTop = "0";
+    
+  }
+  prevScrollpos = currentScrollPos;
+};
